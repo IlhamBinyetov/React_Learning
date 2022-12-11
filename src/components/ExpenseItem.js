53f2a7b8eea35b4ed5e3b82be3ba79e927e9191b
ilhambinyetov <binyetov631@gmail.com>
2022-12-12 01:18:57 +0400

    Card , ExpenseDate and Expenses custom components added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,18 @@
+import ExpenseDate from './ExpenseDate';
+import Card from './Card';
 import './ExpenseItem.css';
 
-function ExpenseItem(props){
 
-
+function ExpenseItem(props) {
 
     return (
-    <div className='expense-item'>
-        {<div>{props.date.toISOString()}</div>}
-        <div className='expense-item__description'>
-            <h2>{props.title}</h2>
-            <div className='expense-item__price'>{props.amount}</div>
-        </div>
-    </div>
+        <Card className='expense-item'>
+            <ExpenseDate date={props.date} />
+            <div className='expense-item__description'>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>${props.amount}</div>
+            </div>
+        </Card>
     );
 
 }
@@ -23,32 +24,3 @@ export default ExpenseItem;
 
 
 
-function saveFilterResult() {
-         
-    let filter = JSON.parse(`@(Json.Serialize(filter))`);
-    $.each(filter, function (a, value) {
-        if (typeof (value) == 'object' && value != null) {
-            let input = $(`.${a} > li > input`);
-            value.forEach(function (a) {
-                input.each(function (key, i) {
-                    let dataId = $(i).data("id");
-                    if (typeof (dataId) == 'string') 
-                    {
-                        dataId = dataId.split(' ')[0];
-                    }
-                  
-                    if (dataId == a) {
-                        console.log(i, dataId, a);
-                        $(i).siblings('label').addClass('atis_custom_select_check_box_selected');
-                        $(i).prop('checked', true);
-                      //  $(i).attr('checked');
-
-                        console.log($(i), 'save result check');
-                        let select = $(i).parent().parent().prev().children();
-                        select.append(`<option selected value=${a}></option>`);
-                    }
-                });
-            });
-        }
-    });
-}
