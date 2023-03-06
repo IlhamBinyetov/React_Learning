cf9513d8d0589bd1614d359c87bb8316f3b950de
IlhamBinyetov <binyetov631@gmail.com>
2023-03-06 12:42:20 +0400

    .

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -22,33 +22,3 @@ export default ExpenseItem;
 
 
 
-
-function saveFilterResult() {
-         
-    let filter = JSON.parse(`@(Json.Serialize(filter))`);
-    $.each(filter, function (a, value) {
-        if (typeof (value) == 'object' && value != null) {
-            let input = $(`.${a} > li > input`);
-            value.forEach(function (a) {
-                input.each(function (key, i) {
-                    let dataId = $(i).data("id");
-                    if (typeof (dataId) == 'string') 
-                    {
-                        dataId = dataId.split(' ')[0];
-                    }
-                  
-                    if (dataId == a) {
-                        console.log(i, dataId, a);
-                        $(i).siblings('label').addClass('atis_custom_select_check_box_selected');
-                        $(i).prop('checked', true);
-                      //  $(i).attr('checked');
-
-                        console.log($(i), 'save result check');
-                        let select = $(i).parent().parent().prev().children();
-                        select.append(`<option selected value=${a}></option>`);
-                    }
-                });
-            });
-        }
-    });
-}
